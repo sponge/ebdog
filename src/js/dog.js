8e985da6c2cfc8cd916de5f1e0a6e9e1bd1ca129
sponge <sponge@d8d.org>
2017-07-09 19:15:41 -0500

    a dog does what a dog wants

---
+++ src/js/dog.js
@@ -0,0 +1,331 @@
+import {Application, Sprite, Rectangle, Texture} from "pixi.js";
+import * as PIXI from "pixi.js";
+
+const IDLE = 0;
+const WAIT = 1
+const MOVE_IN = 2;
+const MOVE_OUT = 3;
+const PUPPY_NAP = 4;
+const STARE_OUT_THE_WINDOW = 5;
+const BARK_FOR_A_WHILE_FOR_NO_REASON = 6;
+const CHASE_TAIL_LIKE_A_DUMMY = 7;
+const TAKE_UP_THE_WHOLE_COUCH = 8;
+const STRETCH_IN_THE_MIDDLE_OF_THE_FLOOR = 9;
+
+function randrange(min, max) {
+    return Math.round(Math.random() * (max-min) + min);
+}
+
+class Dog {
+	constructor(x, y) {
+        this.time = 0;
+
+        this.dogThoughts = {
+            mode: IDLE,
+            dest: {
+                x: 0,
+                y: 0,
+                outY: 0
+            },
+            goalMode: IDLE
+        }
+
+        this.area = new PIXI.Polygon([
+            89, 70,
+            79, 77,
+            58, 77,
+            10, 126,
+            400, 126,
+            353, 75,
+            320, 74,
+            320, 88,
+            270, 88,
+            270, 80,
+            217, 80,
+            200, 80,
+            200, 96,
+            118, 96,
+            118, 70,
+            89, 70
+        ]);
+
+		const dog = Texture.fromImage('images/dog.png');
+		const frames = [
+			new Rectangle(0, 0, 14, 24),
+			new Rectangle(17, 0, 24, 24),
+			new Rectangle(44, 0, 16, 24),
+			new Rectangle(63, 0, 24, 24),
+			new Rectangle(89, 0, 14, 24),
+			new Rectangle(107, 0, 23, 24),
+			new Rectangle(133, 0, 16, 24),
+			new Rectangle(152, 0, 23, 24),
+			new Rectangle(178, 0, 15, 24),
+			new Rectangle(196, 0, 15, 24),
+			new Rectangle(212, 0, 24, 24),
+			new Rectangle(237, 0, 23, 24),
+			new Rectangle(262, 0, 24, 24),
+			new Rectangle(289, 0, 24, 24)
+		]
+		let dogTex = [];
+		for (let rect of frames) {
+			dogTex.push(new Texture(dog, rect));
+		}
+		this.dog = new PIXI.extras.AnimatedSprite(dogTex);
+
+		this.dog.anchor.set(0.5, 0.9);
+
+        if (x === undefined || y === undefined) {
+            let found = false;
+            while (!found) {
+                const x = randrange(10, 400);
+                const y = randrange(70, 126);
+
+                if (this.area.contains(x, y)) {
+                    found = true;
+                    this.dog.x = x;
+                    this.dog.y = y;
+                }
+            }
+        } else {
+            this.dog.x = x;
+            this.dog.y = y;
+        }
+
+        this.states = {
+            [IDLE]: {transition: ()=>{}, update: this.doDogStuff},
+            [MOVE_IN]: {transition: ()=>{}, update: this.updateMoveIn},
+            [MOVE_OUT]: {transition: this.transitionMoveOut, update: this.updateMoveOut},
+            [PUPPY_NAP]: {transition: this.transitionNapTime, update: this.updateNapTime},
+            [STARE_OUT_THE_WINDOW]: {transition: this.transitionWindow, update: this.updateWindow},
+            [BARK_FOR_A_WHILE_FOR_NO_REASON]: {transition: ()=>{}, update: ()=>{}},
+            [CHASE_TAIL_LIKE_A_DUMMY]: {transition: this.transitionTail, update: this.updateTail},
+            [TAKE_UP_THE_WHOLE_COUCH]: {transition: this.transitionCouch, update: this.updateCouch},
+            [STRETCH_IN_THE_MIDDLE_OF_THE_FLOOR]: {transition: this.transitionStretch, update: this.updateStretch},
+            [WAIT]: {transition: ()=>{}, update: ()=>{}},
+        }
+    }
+
+	update(delta) { 
+		this.time += delta;
+        this.states[this.dogThoughts.mode].update.bind(this)();
+	}
+
+    // idle state
+
+    doDogStuff() {
+        console.log("woof woof woof");
+        let {dogThoughts} = this;
+        let {dest} = dogThoughts;
+
+        if (dogThoughts.goalMode != IDLE) {
+            dogThoughts.mode = dogThoughts.goalMode;
+            dogThoughts.goalMode = IDLE;
+        } else {
+            const n = randrange(0, 100);
+
+            dogThoughts.mode = n < 10 ? CHASE_TAIL_LIKE_A_DUMMY :
+                n < 20 ? STARE_OUT_THE_WINDOW :
+                n < 30 ? PUPPY_NAP :
+                n < 40 ? TAKE_UP_THE_WHOLE_COUCH :
+                n < 50 ? STRETCH_IN_THE_MIDDLE_OF_THE_FLOOR :
+                MOVE_OUT;
+        }
+
+        this.states[dogThoughts.mode].transition.bind(this)();
+    }
+
+    // move out state
+
+    transitionMoveOut() {
+        let {dogThoughts} = this;
+        let {dest} = dogThoughts;
+
+        let foundSpotButNotADogSpotButALocationSpot = false;
+
+        while (foundSpotButNotADogSpotButALocationSpot == false) {
+            console.log("sniff sniff");
+            const x = randrange(10, 400);
+            const y = randrange(70, 126);
+            const outY = Math.max(y, randrange(96, 126));
+
+            if (Math.abs(dest.x - x) < 20 || Math.abs(dest.y - y) < 20) {
+                continue;
+            }
+
+            if (!this.area.contains(x, y)) {
+                continue;
+            }
+            
+            foundSpotButNotADogSpotButALocationSpot = true;
+            dest.x = x;
+            dest.y = y;
+            dest.outY = outY;
+        }       
+    }
+
+    updateMoveOut() {
+        let {dog, dogThoughts} = this;
+        let {mode, dest} = dogThoughts;
+
+        if (dest.outY <= dog.y) {
+            dogThoughts.mode = WAIT;
+            setTimeout(() => { dogThoughts.mode = MOVE_IN }, randrange(250, 750))
+            console.log("bark!");
+        }
+        else {
+            this.moveDown();
+        }
+    }
+
+    // move in state
+    
+    updateMoveIn() {
+        let {dog, dogThoughts} = this;
+        let {mode, dest} = dogThoughts;
+
+        if (dog.x == dest.x && dog.y == dest.y) {
+            dogThoughts.mode = WAIT;
+            this.faceRandomDirection();
+            setTimeout(() => { dogThoughts.mode = IDLE }, dogThoughts.goalMode != IDLE ? 0 : randrange(1000, 4000))
+            return;
+        }
+
+        if (dest.x > dog.x) {
+            this.moveRight();
+        }
+        else if (dest.x < dog.x) { 
+            this.moveLeft();
+        }
+        else if (dest.y < dog.y) {
+            this.moveUp();
+        }
+        else {
+            this.moveDown();
+        }
+    }
+
+    // sleepy puppy time
+
+    transitionNapTime() {
+        console.log('yawn');
+        let {dogThoughts} = this;
+        setTimeout(() => { dogThoughts.mode = IDLE }, randrange(5000, 10000));
+    }
+
+    updateNapTime() {
+        console.log('zzz zzz');
+        const rem = Math.floor(this.time / 10) % 2;
+		this.dog.gotoAndStop(rem == 0 ? 12 : 13);
+    }
+
+    // stare out the window
+
+    transitionWindow() {
+        console.log('wag wag wag');
+        let {dog, dogThoughts} = this;
+        let {mode, dest} = dogThoughts;
+
+        const wx = 111, wy = 64
+        if (dog.x != wx || dog.y != wy) {
+            dest.x = wx;
+            dest.y = wy;
+            dest.outY = 104;
+            dogThoughts.mode = MOVE_OUT;
+            dogThoughts.goalMode = STARE_OUT_THE_WINDOW;
+        } else {
+            setTimeout(() => { dogThoughts.mode = IDLE }, randrange(3000, 7000));            
+        }
+    }
+
+    updateWindow() {
+        const rem = Math.floor(this.time / 10) % 2;
+		this.dog.gotoAndStop(rem == 0 ? 8 : 9);
+    }
+
+    // sleep on the couch
+
+    transitionCouch() {
+        console.log('wag wag wag');
+        let {dog, dogThoughts} = this;
+        let {mode, dest} = dogThoughts;
+
+        const wx = 295, wy = 81
+        if (dog.x != wx || dog.y != wy) {
+            dest.x = wx;
+            dest.y = wy;
+            dest.outY = 104;
+            dogThoughts.mode = MOVE_OUT;
+            dogThoughts.goalMode = TAKE_UP_THE_WHOLE_COUCH;
+        } else {
+            setTimeout(() => { dogThoughts.mode = IDLE }, randrange(7000, 14000));            
+        }
+    }
+
+    updateCouch() {
+        const rem = Math.floor(this.time / 10) % 2;
+		this.dog.gotoAndStop(rem == 0 ? 12 : 13);
+    }
+
+    // chase tail
+
+    transitionTail() {
+        console.log('ruff ruff ruff ruff');
+        let {dogThoughts} = this;
+        setTimeout(() => { dogThoughts.mode = IDLE }, randrange(2000, 5000));
+    }
+
+    updateTail() {
+        const rem = Math.floor(this.time / 5) % 4;
+		this.dog.gotoAndStop(rem);
+    }
+
+    // stretch on the floor
+
+    transitionStretch() {
+        console.log('low pitched grumble');
+        let {dogThoughts} = this;
+        setTimeout(() => { dogThoughts.mode = IDLE }, randrange(3000, 6000));
+    }
+
+    updateStretch() {
+        const rem = Math.floor(this.time / 10) % 2;
+		this.dog.gotoAndStop(rem == 0 ? 10 : 11);
+    }
+
+    // utils
+
+    moveLeft() {
+        let {dog} = this;
+        const rem = Math.floor(this.time / 10) % 2;
+		dog.gotoAndStop(rem == 0 ? 1 : 5);
+        dog.x -= 0.5;
+    }
+    
+    moveRight() {
+        let {dog} = this;
+        const rem = Math.floor(this.time / 10) % 2;
+		dog.gotoAndStop(rem == 0 ? 3 : 7);
+        dog.x += 0.5;
+    }
+
+    moveUp() {
+        let {dog} = this;
+        const rem = Math.floor(this.time / 10) % 2;
+		dog.gotoAndStop(rem == 0 ? 2 : 6);
+        dog.y -= 0.5;
+    }
+
+    moveDown() {
+        let {dog} = this;
+        const rem = Math.floor(this.time / 10) % 2;
+		dog.gotoAndStop(rem == 0? 0 : 4);
+        dog.y += 0.5;
+    }
+
+    faceRandomDirection() {
+        const dir = randrange(0,7);
+        this.dog.gotoAndStop(dir);
+    }
+}
+
+export default Dog
---
+++ src/js/main.js
@@ -0,0 +1,53 @@
+import {Application, Sprite, Rectangle, Texture} from "pixi.js";
+import * as PIXI from "pixi.js";
+import Dog from "./dog.js"
+
+document.addEventListener("DOMContentLoaded", () => {
+	let game = new Game();
+	window.game = game;
+});
+
+class Game {
+	constructor() {
+		const app = new Application(415, 128, {backgroundColor : 0x000000});
+		this.app = app;
+		document.body.appendChild(app.view);
+
+		this.time = 0;
+
+		this.bg = Sprite.fromImage('images/bg.png');
+		this.dogs = []
+		
+		PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+		app.renderer.roundPixels = true;
+
+		app.stage.addChild(this.bg);
+
+        let gfx = new PIXI.Graphics();
+		gfx.lineStyle(2, 0xFF0000, 1);
+		app.stage.addChild(gfx);
+
+		this.dogContainer = new PIXI.Container();
+		app.stage.addChild(this.dogContainer);
+
+		for (let i = 0; i < 3; i++) {
+			const dog = new Dog();
+			this.dogs.push( dog );
+			this.dogContainer.addChild(dog.dog);
+		}
+
+        //gfx.drawPolygon(this.dogs[0].area);
+
+		app.ticker.add(this.update.bind(this));
+
+	}
+
+	update(delta) {
+		this.time += delta;
+		for (let dog of this.dogs) {
+			dog.update(delta);
+		}
+
+		this.dogContainer.children.sort((a,b) => { return a.y - b.y });
+	}
+}
